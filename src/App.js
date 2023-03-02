5137e6411004dad66302116324ac420c4dac3a4b
unknown <epeng1994@gmail.com>
2023-03-01 20:57:47 -0500

    added basic state for tasks, form for input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [tasks,setTasks] = useState([])
+  const [newTask,setNewTask] = useState('')
+
+  /*
+    User can enter new tasks via form, hold in state
+    User can check/complete tasks, remove from list?
+    Form to take in input for tasks to be stored within state, div to render out current tasks
+
+  */
+  const onNewTaskSubmit = e =>{
+    
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <input>Enter new task</input>
+      </form>
+
+      {
+        tasks.map(a=>{
+          return(
+            <div>{a}</div>
+          )
+        })
+      }
+
     </div>
   );
 }
