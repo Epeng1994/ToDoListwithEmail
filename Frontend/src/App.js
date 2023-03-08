4d38709435ab633cc64cf26f73f5475714e88308
unknown <epeng1994@gmail.com>
2023-03-08 00:05:43 -0500

    init backend for tasks

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,18 +2,10 @@ import './App.css';
 import { useState } from 'react';
 
 
-
-
 function App() {
   const [tasks,setTasks] = useState(['Do Homework','Laundry'])
   const [newTask,setNewTask] = useState('')
 
-  /*
-    User can enter new tasks via form, hold in state
-    User can check/complete tasks, remove from list?
-    Form to take in input for tasks to be stored within state, div to render out current tasks
-
-  */
   const onNewTaskSubmit = e =>{
     e.preventDefault()
     setTasks([...tasks,newTask])
@@ -30,6 +22,7 @@ function App() {
 
   return (
     <div className = 'App'>
+      <h1>TaskMaster.io</h1>
       <form onSubmit={onNewTaskSubmit}>
         <label for = 'taskEntry'>Enter new task</label>
         <input value = {newTask} name = 'taskEntry' type='text' onChange = {newTaskEntry} onFocus = {()=>setNewTask('')}/>
@@ -45,8 +38,6 @@ function App() {
         }
       </div>
       <button onClick = {clearTasks}>Clear Tasks</button>
-      
-
     </div>
   );
 }
